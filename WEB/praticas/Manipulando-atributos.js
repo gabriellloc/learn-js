b1ceb47e25ec579fdf00be23da600af6e8fec30d
Gabriel <gabrielocdev595@gmail.com>
2025-07-14 13:26:28 -0300

    manipulando atributos

---
+++ WEB/praticas/Manipulando-atributos.js
@@ -0,0 +1,16 @@
+// Manipulando atributos
+
+/* // Desabilitando o input
+const input = document.querySelector("input")
+
+input.setAttribute("disabled", true) */
+
+
+/* // Mundando o tipo do arquivo
+const input = document.querySelector("input")
+input.setAttribute("type", "file") */
+
+/* // Remove o atributo
+const input = document.querySelector("input")
+input.removeAttribute("id") */
+
