546f01a2ad40a61f759eac92b97661d14a9b2bf8
Gabriel <gabrielocdev595@gmail.com>
2025-07-14 13:39:50 -0300

    Atualizei o arquivo html e css

---
+++ WEB/JavaScript.js
@@ -0,0 +1,19 @@
+/* Eventos */
+
+/* Executa sempre que a página for carregada */
+// window.addEventListener("load", () => {
+//     console.log("A página foi carregada")
+// })
+
+
+/* Executa sempre ao click e impede que a página de reload */
+// addEventListener("click", (event) => {
+//     event.preventDefault()
+
+//     console.log(event.target)
+// })
+
+
+/* Eventos em um elemento especifico */
+
+addEventListener
